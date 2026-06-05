c430808279ee02806c6aa3169aa0de1766f2d7fc
NgocHuy2235 <huyngoc2235@gmail.com>
2026-06-05 13:39:00 +0700

    refactor: Đồng bộ định dạng Json trả về cho fronend, xóa pom, gỡ file rác

--- marcus-store-api/src/main/java/com/fpoly/marcusstore/controller/admin/AdminBannerController.java
+++ marcus-store-api/src/main/java/com/fpoly/marcusstore/controller/admin/AdminBannerController.java
@@ -1,16 +1,14 @@
 package com.fpoly.marcusstore.controller.admin;
 
 import com.fpoly.marcusstore.dto.request.BannerRequestDTO;
+import com.fpoly.marcusstore.dto.response.ApiResponse;
 import com.fpoly.marcusstore.dto.response.BannerResponseDTO;
 import com.fpoly.marcusstore.service.BannerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin/banners")
@@ -21,32 +19,32 @@ public class AdminBannerController {
 
     //lấy tất cả banner
     @GetMapping
-    public ResponseEntity<List<BannerResponseDTO>> getAll() {
-        return ResponseEntity.ok(bannerService.getAll());
+    public ApiResponse<List<BannerResponseDTO>> getAll() {
+        return ApiResponse.success(bannerService.getAll());
     }
 
     //lấy chi tiết 1 banner
     @GetMapping("/{id}")
-    public ResponseEntity<BannerResponseDTO> getOne(@PathVariable Integer id) {
-        return ResponseEntity.ok(bannerService.getOne(id));
+    public ApiResponse<BannerResponseDTO> getOne(@PathVariable Integer id) {
+        return ApiResponse.success(bannerService.getOne(id));
     }
 
     // thêm banner mới
     @PostMapping
-    public ResponseEntity<BannerResponseDTO> add(@Valid @RequestBody BannerRequestDTO req) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(bannerService.add(req));
+    public ApiResponse<BannerResponseDTO> add(@Valid @RequestBody BannerRequestDTO req) {
+        return ApiResponse.success(bannerService.add(req));
     }
 
     // sửa banner
     @PutMapping("/{id}")
-    public ResponseEntity<BannerResponseDTO> update( @PathVariable Integer id, @Valid @RequestBody BannerRequestDTO req) {
-        return ResponseEntity.ok(bannerService.update(id, req));
+    public ApiResponse<BannerResponseDTO> update( @PathVariable Integer id, @Valid @RequestBody BannerRequestDTO req) {
+        return ApiResponse.success(bannerService.update(id, req));
     }
 
     //xóa mềm banner
     @DeleteMapping("/{id}")
-    public ResponseEntity<Map<String, String>> remove(@PathVariable Integer id) {
+    public ApiResponse<String> remove(@PathVariable Integer id) {
         bannerService.remove(id);
-        return ResponseEntity.ok(Map.of("message", "Xóa banner thành công"));
+        return ApiResponse.success("Xóa banner thành công");
     }
 }
