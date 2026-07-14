4c45180f884dc88e18fa86e7b381e008f01fc2e4
Ngoc01012 <doanvanhuy01012006@gmail.com>
2026-07-14 17:26:49 +0700

    chore: WIP - Thêm ẩn GG khi đăng ký qua link

--- marcus-store-frontend/src/views/auth/Register.vue
+++ marcus-store-frontend/src/views/auth/Register.vue
@@ -89,21 +89,25 @@
           🎁 Bạn sẽ nhận ngay <strong>voucher 100K</strong> sau khi hoàn tất đăng ký!
         </div>
 
-        <div class="social-login">
-          <button type="button" class="social-btn social-gg" @click="registerGoogle">
-            <span class="social-icon">
-              <svg width="20" height="20" viewBox="0 0 24 24">
-                <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92a5.06 5.06 0 0 1-2.2 3.32v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.1z"/>
-                <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.99.66-2.25 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.85A10.98 10.98 0 0 0 12 23z"/>
-                <path fill="#FBBC05" d="M5.84 14.1a6.6 6.6 0 0 1 0-4.2V7.05H2.18a11 11 0 0 0 0 9.9l3.66-2.85z"/>
-                <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.05l3.66 2.85C6.71 7.3 9.14 5.38 12 5.38z"/>
-              </svg>
-            </span>
-            Đăng ký bằng Google
-          </button>
-        </div>
+ <div v-if="!isNewsletterSignup" class="social-login">
+  <button type="button" class="social-btn social-gg" @click="registerGoogle">
+    <span class="social-icon">
+      <svg width="20" height="20" viewBox="0 0 24 24">
+        <path fill="#4285F4"
+          d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92a5.06 5.06 0 0 1-2.2 3.32v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.1z"/>
+        <path fill="#34A853"
+          d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.99.66-2.25 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.85A10.98 10.98 0 0 0 12 23z"/>
+        <path fill="#FBBC05"
+          d="M5.84 14.1a6.6 6.6 0 0 1 0-4.2V7.05H2.18a11 11 0 0 0 0 9.9l3.66-2.85z"/>
+        <path fill="#EA4335"
+          d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.05l3.66 2.85C6.71 7.3 9.14 5.38 12 5.38z"/>
+      </svg>
+    </span>
+    Đăng ký bằng Google
+  </button>
+</div>
 
-        <div class="divider"><span>hoặc điền thông tin bên dưới</span></div>
+        <div  v-if="!isNewsletterSignup" class="divider"><span>hoặc điền thông tin bên dưới</span></div>
 
         <form @submit.prevent="handleRegister">
           <div class="grid-form">
